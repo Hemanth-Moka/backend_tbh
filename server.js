80631333ac4aa819b1d3070ef9dcd9df71a4a8fe
SiriPriya <2300030401@kluniversity.in>
2025-07-30 01:48:58 +0530

    added join backend

--- server.js
+++ server.js
@@ -3,28 +3,25 @@ const cors = require('cors');
 require('dotenv').config();
 const path = require('path');
 
+// Initialize app
+const app = express();
+
 app.use(cors());
 app.use(express.json());
-
-
 
 // Routes
 const loginRoute = require('./routes/login');
 const joinRoute = require('./routes/join');
 
-
 app.use('/login', loginRoute);
 app.use('/join', joinRoute);
-
 
 // Static folder for uploaded images
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-
 // Optional: Multer error handler
 app.use((err, req, res, next) => {
   if (err.name === 'MulterError') {
-    // Handle multer-specific errors
     return res.status(400).json({ error: err.message });
   }
   next(err);
